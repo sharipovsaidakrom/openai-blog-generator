548753e69b0266cee62b073d67c2b5de0a0b7df6
God of Cyber <140199969+sharipovsaidakrom@users.noreply.github.com>
2025-05-12 08:58:53 +0500

    Add files via upload

---
+++ blog_generator.py
@@ -0,0 +1,44 @@
+import openai
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+# create a .env file on the folder and write the the code below
+# API_KEY='your api token. In .env file, there shouldn't be any empty space'
+openai.api_key = config['API_KEY']
+
+def generate_blog(paragraph_topic):
+  response = openai.completions.create(
+    model = 'gpt-3.5-turbo-instruct',
+    prompt = 'Write a paragraph about the following topic. ' + paragraph_topic,
+    max_tokens = 400,
+    temperature = 0.3
+  )
+
+  retrieve_blog = response.choices[0].text
+
+  return retrieve_blog
+
+print(generate_blog('Why NYC is better than your city?'))
+
+keep_writing = True
+
+while keep_writing:
+  answer = input('Write a paragraph? Y for yes, anything else for no.')
+  if(answer == 'Y'):
+    paragraph_topic = input('What should this paragraph talk about? ')
+    print(generate_blog(paragraph_topic))
+  else:
+    keep_writing = False
+
+
+
+
+
+
+
+
+
+
+
+
